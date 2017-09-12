d22c7bbe8eb2d18e1c56ec88025fb75e0a045a58
webil13 <p.hlavacik@gmail.com>
2017-09-12 14:16:05 +0200

    navbar, materializecss, jquery

---
+++ src/restaurants/views.py
@@ -0,0 +1,34 @@
+import random
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+# function based view 
+def home(request):
+    num = None
+    some_list = [
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+        random.randint(0, 100000000),
+    ]
+    condition_bool_item = False
+    if condition_bool_item:
+        num = random.randint(0, 100000000)
+    context = {
+        "num": num,
+        "some_list": some_list
+    }
+
+    return render(request, "home.html", context)
+
+def about(request):
+    context = {
+    }
+
+    return render(request, "about.html", context)
+
+def contact(request):
+    context = {
+    }
+
+    return render(request, "contact.html", context)
